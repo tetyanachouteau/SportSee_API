7bf8b911fa4e2d6ce2d73a4561c5ef877d6905c6
Tetyana <tetyana.chouteau@gmail.com>
2023-11-06 16:30:28 +0100

    Diverces installations

---
+++ Front-End-Dashboard/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//import axios from 'axios';
+function App() {
+  return (
+    <div>
+      <h1>Mon application React</h1>
+      <p>Ceci est un paragraphe HTML.</p>
+    </div>
+  );
+}
+
+export default App;
+
+
+
--- README.md
+++
@@ -1,1 +0,0 @@
-# SportSee_API
